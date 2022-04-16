a9d7c38e9a76622cf3e02cd0dc8dae92a6daa1d4
unaisshemim <unaisshemim@gmail.com>
2022-04-16 07:32:15 +0530

    Configure React app for deployment to GitHub Pages

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MainScreen from './Screens/MainScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <MainScreen/>
     </View>
   );
 }
@@ -13,8 +13,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+   
   },
 });
